ca4709c059a486f4eb71be09a93f0bd66c374fce
Jimmy Tran <47673723+haboohanu@users.noreply.github.com>
2022-07-14 10:36:16 -0700

    dropdown

--- src/app/features/appointments/appointments.component.ts
+++ src/app/features/appointments/appointments.component.ts
@@ -1,53 +1,137 @@
-import { Component } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
-import { RouterExtensions } from '@nativescript/angular'
-import { DatePicker, Page } from '@nativescript/core';
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { RouterExtensions } from "@nativescript/angular";
+import { DatePicker, EventData, Page, TimePicker } from "@nativescript/core";
+import { SelectedIndexChangedEventData } from "nativescript-drop-down";
+import { CourtModel } from "~/app/core/models/court.model";
+import { CourtService } from "~/app/core/services/court.service";
+import { ValueList, ValueItem, DropDown } from "nativescript-drop-down";
+import {
+  DatePickerFieldDirective,
+  TimePickerFieldDirective,
+} from "@nativescript/datetimepicker/angular";
 
 @Component({
-    moduleId: module.id,
-    selector: 'ns-appointments',
-    templateUrl: 'appointments.component.html'
+  moduleId: module.id,
+  selector: "ns-appointments",
+  templateUrl: "appointments.component.html",
 })
 export class AppointmentsComponent {
+  public selectedIndex: number;
+  public items: Array<string>;
+  selectedDate: Date;
+  selectedTime: Date;
+  courts: CourtModel[] = [];
+  courtItems: ValueItem<CourtModel>[] = [];
 
-    constructor(
-        private activatedRoute: ActivatedRoute,
-        private routerExtensions: RouterExtensions,
-        private page: Page,
+  public itemSource: ValueList<CourtModel>;
+
+  iitemSource = new ValueList<string>([
+    { value: "FL", display: "Florida" },
+    { value: "MI", display: "Michigan" },
+  ]);
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private routerExtensions: RouterExtensions,
+    private page: Page,
+    private courtService: CourtService
   ) {
     page.actionBarHidden = true;
+    this.items = [];
+    for (var i = 0; i < 5; i++) {
+      this.items.push("data item " + i);
+    }
   }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+    this.getCourts();
+  }
 
-    minDate: Date = new Date(1975, 0, 29);
-    maxDate: Date = new Date(2045, 4, 12);
+  getCourts() {
+    this.courtService.getCourts().subscribe((res) => {
+      let dd = this.page.getViewById<DropDown>("dd");
+      for (let c of res.results) {
+        let _court: CourtModel = {
+          id: c.id,
+          courtName: c.full_name,
+          url: c.url,
+          jurisdiction: c.jurisdiction,
+        };
+        let item: ValueItem<CourtModel> = {
+          value: _court,
+          display: _court.courtName,
+        };
+        this.courtItems.push(item);
+      }
+      console.log("COURITEMS", this.courtItems);
+      this.itemSource = new ValueList<CourtModel>(this.courtItems);
+      console.log("ITEMSOURCE", this.itemSource);
+      //dd.items= itemSource;
+    });
+  }
 
-    onDatePickerLoaded(args) {
-        // const datePicker = args.object as DatePicker;
-    }
+  onPickDate(args: EventData) {}
 
-    onDateChanged(args) {
-        console.log("Date New value: " + args.value);
-        console.log("Date value: " + args.oldValue);
-    }
+  onPickTime(args: EventData) {}
 
-    onDayChanged(args) {
-        console.log("Day New value: " + args.value);
-        console.log("Day Old value: " + args.oldValue);
-    }
+  minDate: Date = new Date(1975, 0, 29);
+  maxDate: Date = new Date(2045, 4, 12);
 
-    onMonthChanged(args) {
-        console.log("Month New value: " + args.value);
-        console.log("Month Old value: " + args.oldValue);
-    }
+  onDatePickerLoaded(args) {
+    // const datePicker = args.object as DatePicker;
+  }
 
-    onYearChanged(args) {
-        console.log("Year New value: " + args.value);
-        console.log("Year Old value: " + args.oldValue);
-    }
+  onDateChanged(args) {
+    console.log("Date New value: " + args.value);
+    console.log("Date value: " + args.oldValue);
+  }
 
+  onDayChanged(args) {
+    console.log("Day New value: " + args.value);
+    console.log("Day Old value: " + args.oldValue);
+  }
+
+  onMonthChanged(args) {
+    console.log("Month New value: " + args.value);
+    console.log("Month Old value: " + args.oldValue);
+  }
+
+  onYearChanged(args) {
+    console.log("Year New value: " + args.value);
+    console.log("Year Old value: " + args.oldValue);
+  }
+
+  public onchange(args: SelectedIndexChangedEventData) {
+    console.log(
+      `Drop Down selected index changed from ${args.oldIndex} to ${args.newIndex}`
+    );
+    console.log("selected value:", this.itemSource.getValue(args.newIndex));
+  }
+
+  public onopen() {
+    console.log("Drop Down opened.");
+  }
+
+  public onclose() {
+    console.log("Drop Down closed.");
+  }
+
+  getFormattedDate(date: Date): string {
+    const d = date.getDate();
+    const m = date.getMonth() + 1;
+    const y = date.getFullYear();
+    return (m < 10 ? "0" : "") + m + "." + (d < 10 ? "0" : "") + d + "." + y;
+  }
+
+  getFormattedTime(date: Date): string {
+    const h = date.getHours();
+    const m = date.getMinutes();
+    return (h < 10 ? "0" : "") + h + ":" + (m < 10 ? "0" : "") + m;
+  }
+
+  onSave() {
+    console.log(this.getFormattedDate(this.selectedDate));
+    console.log(this.getFormattedTime(this.selectedTime));
+  }
 }
-
-
--- src/app/features/appointments/appointments.module.ts
+++ src/app/features/appointments/appointments.module.ts
@@ -3,10 +3,10 @@ import { NativeScriptCommonModule } from '@nativescript/angular'
 import { AppointmentsRoutingModule } from './appointments-routing.module'
 import { AppointmentsComponent } from './appointments.component'
 import { NativeScriptDateTimePickerModule } from "@nativescript/datetimepicker/angular";
-
+import { DropDownModule } from "nativescript-drop-down/angular";
 
 @NgModule({
-  imports: [NativeScriptCommonModule, AppointmentsRoutingModule, NativeScriptDateTimePickerModule],
+  imports: [NativeScriptCommonModule, AppointmentsRoutingModule, NativeScriptDateTimePickerModule, DropDownModule],
   declarations: [AppointmentsComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
